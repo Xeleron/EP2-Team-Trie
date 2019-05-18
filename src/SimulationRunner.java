8576f2ce2e61a6e756ec6eede1b6c634be2ca67d
Marko Cubela <e11808231@student.tuwien.ac.at>
2019-05-18 16:56:49 +0200

    implemented add

--- src/SimulationRunner.java
+++ src/SimulationRunner.java
@@ -7,9 +7,9 @@ public class SimulationRunner {
         for (int i = 0; i < 6; i++) {
             Trie trie = new Trie();
             try (Scanner s = new Scanner(new File("./data/initial-collectors-" + i + ".csv"), "UTF-8").useDelimiter(";")) {
-
+                trie.add(s.next(), Integer.parseInt(s.next()), Integer.parseInt(s.next()));
             } catch (FileNotFoundException e) {
-                System.out.println("initial-collectors-" + i + ".csv wurde nicht gefunden");
+                System.out.println("Error bei initial-collectors-" + i + ".csv");
                 System.exit(1);
             }
         }
--- src/Trie.java
+++ src/Trie.java
@@ -3,17 +3,43 @@ import java.util.Map;
 
 public class Trie {
     private TrieNode root;
+    private int currentSize;
+    private int maximumSize;
+    private int minimumSize;
 
     public Trie() {
         root = new TrieNode();
     }
 
+    public void add(String id, int totalRequests, int partialRequests) {
+        TrieNode current = root;
+        for (int i = 0; i < id.length(); i++) {
+            current = current.getChildren().computeIfAbsent(id.charAt(i), c -> new TrieNode());
+        }
+        current.setID(true);
+        current.setTotalRequests(totalRequests);
+        current.setPartialRequests(partialRequests);
+        currentSize++;
+        maximumSize++;
+        minimumSize++;
+    }
+
     private class TrieNode {
         private final Map<Character, TrieNode> children = new HashMap<>();
         private boolean ID;
+        private int totalRequests;
+        private int partialRequests;
 
         public Map<Character, TrieNode> getChildren() {
             return children;
+        }
+
+        public void setPartialRequests(int partialRequests) {
+            this.partialRequests = partialRequests;
+        }
+
+        public void setTotalRequests(int totalRequests) {
+            this.totalRequests = totalRequests;
         }
 
         public boolean isID() {
