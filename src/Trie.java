a6f10c69a01bb8e0dfe4fa910e87bba45b27417f
Marko Cubela <e11808231@student.tuwien.ac.at>
2019-05-20 16:15:18 +0200

    almost complete

--- src/Trie.java
+++ src/Trie.java
@@ -1,53 +1,114 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Trie {
-    private TrieNode root;
+    private final TrieNode root = new TrieNode();
+    private final Map<String, Integer> activityMap = new TreeMap<>();
     private int currentSize;
     private int maximumSize;
     private int minimumSize;
+    private int totalNetworkRequests;
+    private String lastDeactivated;
+    private String lastActivated;
 
-    public Trie() {
-        root = new TrieNode();
+    public static TrieNode getCollector(String id, TrieNode root) {
+        TrieNode collector = root;
+        for (int i = 0; i < id.length(); i++) {
+            TrieNode previous = collector;
+            collector = collector.children.get(id.charAt(i));
+            if (collector == null) return getFirstCollector(previous);
+        }
+        return collector;
+    }
+
+    public static boolean delete(String id, TrieNode root) {
+        if (id.length() == 1) {
+            root.children.remove(id.charAt(0));
+            return root.children.isEmpty();
+        }
+        if (delete(id.substring(1), root.children.get(id.charAt(0)))) root.children.remove(id.charAt(0));
+        return root.children.isEmpty();
+    }
+
+    public static ArrayList<TrieNode> toList(TrieNode root) {
+        ArrayList<TrieNode> list = new ArrayList<>();
+        for (TrieNode value : root.children.values()) {
+            if (value.ID != null) list.add(value);
+            else list.addAll(toList(value));
+        }
+        return list;
+    }
+
+    private static TrieNode getFirstCollector(TrieNode root) {
+        Iterator<Map.Entry<Character, TrieNode>> iterator = root.children.entrySet().iterator();
+        return iterator.hasNext() ? getFirstCollector(iterator.next().getValue()) : root;
     }
 
     public void add(String id, int totalRequests, int partialRequests) {
         TrieNode current = root;
-        for (int i = 0; i < id.length(); i++) {
-            current = current.getChildren().computeIfAbsent(id.charAt(i), c -> new TrieNode());
-        }
-        current.setID(true);
-        current.setTotalRequests(totalRequests);
-        current.setPartialRequests(partialRequests);
+        for (int i = 0; i < id.length(); i++)
+            current = current.children.computeIfAbsent(id.charAt(i), c -> new TrieNode());
+        current.ID = id;
+        current.totalRequests = totalRequests;
+        current.partialRequests = partialRequests;
         currentSize++;
         maximumSize++;
         minimumSize++;
     }
 
+    public void query(String id) {
+        TrieNode collector = getCollector(id, root);
+        collector.totalRequests++;
+        collector.partialRequests++;
+        totalNetworkRequests++;
+        if (collector.totalRequests >= 1000) {
+            delete(collector.ID, root);
+            lastDeactivated = collector.ID;
+            add(id, 0, 0);
+        } else if (collector.partialRequests >= 250) {
+            collector.partialRequests = 0;
+            add(id, 0, 0);
+            currentSize++;
+            maximumSize = Math.max(currentSize, maximumSize);
+            lastActivated = id;
+        }
+        if (totalNetworkRequests % 500000 == 0) disableInactive();
+    }
+
+    private void disableInactive() {
+        for (TrieNode node : toList(root)) {
+            if (node.totalRequests == activityMap.getOrDefault(node.ID, 0)) {
+                currentSize--;
+                minimumSize = Math.min(currentSize, minimumSize);
+                delete(node.ID, root);
+            } else {
+                activityMap.put(node.ID, node.totalRequests);
+            }
+        }
+    }
+
+    public int getMinimumSize() {
+        return minimumSize;
+    }
+
+    public int getMaximumSize() {
+        return maximumSize;
+    }
+
+    public String getLastDeactivated() {
+        return lastDeactivated;
+    }
+
+    public String getLastActivated() {
+        return lastActivated;
+    }
+
     private class TrieNode {
-        private final Map<Character, TrieNode> children = new HashMap<>();
-        private boolean ID;
+        private final Map<Character, TrieNode> children = new TreeMap<>();
+        private String ID;
         private int totalRequests;
         private int partialRequests;
-
-        public Map<Character, TrieNode> getChildren() {
-            return children;
-        }
-
-        public void setPartialRequests(int partialRequests) {
-            this.partialRequests = partialRequests;
-        }
-
-        public void setTotalRequests(int totalRequests) {
-            this.totalRequests = totalRequests;
-        }
-
-        public boolean isID() {
-            return ID;
-        }
-
-        public void setID(boolean ID) {
-            this.ID = ID;
-        }
     }
 }
